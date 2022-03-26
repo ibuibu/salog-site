eff135d67467d74e53bd353efa9f4b2979f47fcb
ibuibu <ibuibu69@gmail.com>
2022-03-26 22:12:18 +0900

    Be responsive

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,25 @@ import screen_check from './assets/screen_check.png';
 function App() {
   return (
     <main className="text-center text-gray-600 border-salog-pink border-8">
-      <section className="p-12">
+      <section className="p-8 md:p-12">
         <>
-          <div className="flex justify-center">
-            <img src={screen_map} className="h-120 m-8"></img>
-            <div className="m-8 flex items-center">
+          <div className="md:flex md:justify-center">
+            <img
+              src={screen_map}
+              className="hidden md:block md:h-120 md:m-8"
+            ></img>
+            <div className="md:m-8 flex items-center">
               <div>
                 <img src={logo_svg} className="h-40 inline"></img>
-                <h1 className="text-3xl m-4">かんたんサウナ記録アプリ</h1>
-                <p className="m-4">
+                <h1 className="text-xl md:text-3xl md:m-4">
+                  かんたんサウナ記録アプリ
+                </h1>
+                <p className="text-sm md:text-base m-4">
                   サウナに着いたらチェックイン！
                   <br />
                   サウナの記録をかんたんに残そう
                 </p>
-                <p className="mt-12 text-salog-pink font-bold text-lg">
+                <p className="mt-12 text-salog-pink font-bold text-md md:text-lg">
                   ただいまアプリ申請中・・・！！！
                 </p>
               </div>
@@ -29,28 +34,28 @@ function App() {
           </div>
         </>
       </section>
-      <section className="p-12 bg-salog-blue text-white">
+      <section className="p-8 md:p-12 bg-salog-blue text-white">
         <>
           <h1 className="text-4xl mb-8 font-bold">使い方</h1>
-          <div className="flex justify-around">
-            <div className="w-56">
+          <div className="md:flex md:justify-around">
+            <div className="m-auto w-56">
               <p className="text-2xl m-4 font-bold">探す</p>
               <img src={screen_map}></img>
-              <p className="text-base my-4">地図からサウナを探そう！</p>
+              <p className="text-base mt-4 mb-12">地図からサウナを探そう！</p>
             </div>
-            <div className="w-56">
+            <div className="m-auto w-56">
               <p className="text-2xl m-4 font-bold">チェックイン</p>
               <img src={screen_check}></img>
-              <p className="text-base my-4">
+              <p className="text-base mt-4 mb-12">
                 サウナに着いたら、
                 <br />
                 チェックインボタンをタップ！
               </p>
             </div>
-            <div className="w-56">
+            <div className="m-auto w-56">
               <p className="text-2xl m-4 font-bold">サログを確認</p>
               <img src={screen_log}></img>
-              <p className="text-base my-4">
+              <p className="text-base mt-4 mb-12">
                 最近何回サウナに行ったか、
                 <br />
                 一目瞭然！
@@ -59,11 +64,14 @@ function App() {
           </div>
         </>
       </section>
-      <section className="p-12">
+      <section className="p-8 md:p-12">
         <>
           <h1 className="text-4xl mb-8 font-bold">特徴</h1>
-          <div className="flex justify-center my-8">
-            <img src={screen_many} className="h-110 mx-20"></img>
+          {/* md */}
+          <div className="hidden md:block flex justify-center my-8">
+            <div className="m-auto w-56">
+              <img src={screen_many} className="h-110 mx-20"></img>
+            </div>
             <div className="mx-8 my-16">
               <h1 className="text-3xl font-bold">
                 東京約500箇所のサウナ位置データ
@@ -71,7 +79,7 @@ function App() {
               <p className="m-4 text-lg">エリアは順次拡大中！</p>
             </div>
           </div>
-          <div className="flex justify-center my-8">
+          <div className="hidden md:block flex justify-center my-8">
             <div className="mx-8 my-16">
               <h1 className="text-3xl font-bold">スタンプラリー</h1>
               <p className="m-4 text-lg">
@@ -80,11 +88,38 @@ function App() {
                 近くのサウナを制覇しよう。
               </p>
             </div>
-            <img src={screen_stamp} className="h-110 mx-20"></img>
+            <div className="m-auto w-56">
+              <img src={screen_stamp} className="h-110 mx-20"></img>
+            </div>
+          </div>
+          {/* sm */}
+          <div className="md:hidden my-8">
+            <div className="m-auto w-56">
+              <h1 className="text-2xl font-bold m-4">
+                東京約500箇所の
+                <br />
+                サウナ位置データ
+              </h1>
+              <img src={screen_many} className="h-110"></img>
+              <p className="m-4 text-sm mb-8">エリアは順次拡大中！</p>
+            </div>
+          </div>
+          <div className="md:hidden my-8">
+            <h1 className="text-2xl font-bold m-4">スタンプラリー</h1>
+            <div className="m-auto w-56">
+              <img src={screen_stamp} className="h-110"></img>
+              <p className="m-4 text-sm mb-8">
+                チェックインで
+                <br />
+                スタンプをゲット。
+                <br />
+                近くのサウナを制覇しよう。
+              </p>
+            </div>
           </div>
         </>
       </section>
-      <section className="p-12 bg-gray-100">
+      <section className="p-8 bg-gray-100">
         <>
           <h1 className="text-4xl mb-8">SPECIAL THANKS</h1>
           <a
@@ -95,8 +130,10 @@ function App() {
           >
             サログさん
           </a>
-          <p className="m-2">
-            アプリ名の使用許可をいただきました！ありがとうございます！
+          <p className="m-2 text-sm md:text-base">
+            アプリ名の使用許可をいただきました！
+            <br className="md:hidden" />
+            ありがとうございます！
           </p>
         </>
       </section>

d191d08c1b8e41f9eed5264e6a197ab9badf2426
ibuibu <ibuibu69@gmail.com>
2022-03-26 19:12:21 +0900

    Add Privacy Policy

---
+++ src/Pp.tsx
@@ -0,0 +1,7 @@
+export const Pp = () => {
+  return (
+    <main className="text-center text-gray-700">
+      <p>プライバシーポリシー</p>
+    </main>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import 'virtual:windi.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "virtual:windi.css";
+import { Pp } from "./Pp";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/privacy-policy" element={<Pp />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
